e86c60fe322c28c7943777556ce9b9a0cee21ed0
AbdulManan4898 <109536229+AbdulManan4898@users.noreply.github.com>
2026-07-13 18:36:43 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,22 @@
+// App.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { ThemeProvider } from './src/theme/ThemeContext';
+import AppNavigator from './src/components/navigation/AppNavigator';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from 'react-native';
 
+const App = () => {
+  return (
+    <ThemeProvider>
+      <SafeAreaProvider>
+        <StatusBar barStyle="dark-content" />
+        <NavigationContainer>
+          <AppNavigator />
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </ThemeProvider>
+  );
+};
+
+export default App;

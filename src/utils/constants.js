740bb9e75bf4674edf627412c346e3c60814f807
AbdulManan4898 <109536229+AbdulManan4898@users.noreply.github.com>
2026-07-13 18:44:58 +0500

    Update and rename constants.ts to constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,21 @@
+// src/utils/constants.js
+export const APP_CONFIG = {
+  APP_NAME: 'Status Saver',
+  VERSION: '1.0.0',
+  FOLDER_NAME: 'StatusSaver',
+};
+
+export const STORAGE_KEYS = {
+  THEME: '@theme_preference',
+  SAVED_STATUSES: '@saved_statuses',
+};
+
+export const FILE_TYPES = {
+  IMAGE: 'image',
+  VIDEO: 'video',
+};
+
+export const STATUS_FOLDERS = {
+  WHATSAPP: '/storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/.Statuses',
+  BUSINESS: '/storage/emulated/0/Android/media/com.whatsapp.w4b/WhatsApp Business/Media/.Statuses',
+};
